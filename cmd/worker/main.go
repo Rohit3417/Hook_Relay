c21a4611b0bd0d212691cdbeca5436e8f49a2705
Rohit <mandalerohit54@gmail.com>
2026-09-10 12:11:05 +0530

    basic worker of worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -4,10 +4,17 @@ import (
 	"Hook_Relay2/internal/adapters/redisqueue"
 	"Hook_Relay2/internal/adapters/storage"
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/joho/godotenv"
+)
+
+const (
+	consumerGroup = "dispatcher_group"
+	batchSize     = 10
 )
 
 func main() {
@@ -15,17 +22,18 @@ func main() {
 	if err != nil {
 		log.Printf("No .env found")
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+
+	startupCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	pool, err := storage.NewPool(ctx)
+	pool, err := storage.NewPool(startupCtx)
 	if err != nil {
 		log.Fatalf("%v", err)
 		return
 	}
 	defer pool.Close()
 
-	client, err := redisqueue.NewRedisConnection(ctx)
+	client, err := redisqueue.NewRedisConnection(startupCtx)
 	if err != nil {
 		log.Fatalf("%v", err)
 		return
@@ -33,4 +41,29 @@ func main() {
 	defer client.Close()
 
 	log.Println("worker started")
+	consumerName := os.Getenv("WORKER_NAME")
+	if consumerName == "" {
+		consumerName = fmt.Sprintf("worker-%d", os.Getpid())
+	}
+
+	queue := redisqueue.NewClient(client)
+	if err := queue.EnsureGroup(startupCtx, consumerGroup); err != nil {
+		log.Println(err)
+		return
+	}
+
+	//We cannot set timeout for this ctx as we require it in ReadPending function which runs forever
+	loopCtx := context.Background()
+	for {
+		events, err := queue.ReadPending(loopCtx, consumerGroup, consumerName, batchSize)
+		if err != nil {
+			log.Printf("read pending failed: %v", err)
+			time.Sleep(1 * time.Second)
+		}
+
+		for _, event := range events {
+			//handle(event)
+		}
+	}
+
 }
